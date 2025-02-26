d944d514699e9fedbd86576a4a9e23b3dafe8b93
shashikant kumar <shashi94goswami@gmail.com>
2025-02-26 23:01:41 +0530

    added ui for celery APi

--- js/promise.js
+++ js/promise.js
@@ -1,93 +1,100 @@
 const STATE = {
     PENDING: 'pending',
-    FULLFILLED: 'fullfilled',
-    REJECTED: 'rejeacted'
-
-}
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected'
+};
 
 class MyPromise {
-    #state = STATE.PENDING
-    #value = null
-    #fullfilledCallbacks = []
-    #rejectedCallbacks = []
+    #state = STATE.PENDING;
+    #value = null;
+    #fulfilledCallbacks = [];
+    #rejectedCallbacks = [];
+
     constructor(executorFun) {
         try {
             executorFun(
                 value => this.#resolve(value),
-                value => this.#reject(value),
+                reason => this.#reject(reason)
             );
         } catch (error) {
-            this.#reject(error)
+            this.#reject(error);
         }
-
-
     }
+
     then(onFulfilled, onRejected) {
         return new MyPromise((resolve, reject) => {
-            const fullfilledCallback = () => {
-                if (!onFulfilled) return resolve(this.#value)
+            const fulfilledCallback = () => {
+                if (!onFulfilled) return resolve(this.#value);
                 queueMicrotask(() => {
                     try {
-                        const value = onFulfilled(this.#value)
-                        resolve(value)
+                        const value = onFulfilled(this.#value);
+                        resolve(value);
                     } catch (error) {
-
-                        reject(error)
-
+                        reject(error);
                     }
-                })
-            }
+                });
+            };
 
             const rejectedCallback = () => {
-                if (!onRejected) return reject(this.#value)
+                if (!onRejected) return reject(this.#value);
                 queueMicrotask(() => {
                     try {
-                       const value = onRejected(this.#value)
-                       resolve(value)
-                    } catch (err) {
-                      reject(err)
+                        const value = onRejected(this.#value);
+                        resolve(value);
+                    } catch (error) {
+                        reject(error);
                     }
-                })
-            }
-            switch(this.#state){
+                });
+            };
+
+            switch (this.#state) {
                 case STATE.PENDING:
-                    this.#fullfilledCallbacks.push(fullfilledCallback);
-                    this.#rejectedCallbacks.push(rejectedCallback)
+                    this.#fulfilledCallbacks.push(fulfilledCallback);
+                    this.#rejectedCallbacks.push(rejectedCallback);
                     break;
-                case STATE.FULLFILLED:
-                    fullfilledCallback();
+                case STATE.FULFILLED:
+                    fulfilledCallback();
                     break;
                 case STATE.REJECTED:
                     rejectedCallback();
                     break;
                 default:
-                    throw new Error("Unexpected promise state")
+                    throw new Error('Unexpected promise state');
             }
-        })
-    }
-    catch(onRejected){
-        return this.then(null,onRejected)
+        });
     }
 
-    get state(){
-        return this.#value
+    catch(onRejected) {
+        return this.then(null, onRejected);
     }
 
-    get value(){
-        return this.#value
-    }
-
-    #resolve(value){
+    #resolve(value) {
+        if (this.#state !== STATE.PENDING) return;
         this.#value = value;
-        this.#state=STATE.FULLFILLED;
-        this.#fullfilledCallbacks.forEach(callback => callback())
+        this.#state = STATE.FULFILLED;
+        this.#fulfilledCallbacks.forEach(callback => callback());
+        this.#fulfilledCallbacks = [];
+        this.#rejectedCallbacks = [];
     }
-    #reject(value){
-        this.#value = value;
+
+    #reject(reason) {
+        if (this.#state !== STATE.PENDING) return;
+        this.#value = reason;
         this.#state = STATE.REJECTED;
-        this.#rejectedCallbacks.forEach(callback=> callback)
+        this.#rejectedCallbacks.forEach(callback => callback());
+        this.#fulfilledCallbacks = [];
+        this.#rejectedCallbacks = [];
+    }
+
+    get state() {
+        return this.#state;
+    }
+
+    get value() {
+        return this.#value;
     }
 }
+
 
 
 new MyPromise((res,rej)=>{
