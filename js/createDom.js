9d1b25abf2a31703080f34a0fea9492ff2e849af
shashikant kumar <shashi94goswami@gmail.com>
2025-03-07 22:13:24 +0530

    added create dom

---
+++ js/createDom.js
@@ -0,0 +1,20 @@
+
+
+function createDom(root) {
+
+    const node = document.createElement(root.type)
+
+    if (root.attributes != null) {
+        for (const [attribute, value] of Object.entries(root.attributes)) {
+            node.setAttribute(attribute, value)
+        }
+    }
+
+    root.children?.forEach(child => {
+        node.append(typeof child === 'string' ? child : createDom(child))
+    })
+
+    return node;
+}
+
+
