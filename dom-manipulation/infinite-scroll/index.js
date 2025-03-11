7a5d15e36ab62d784ef04a8b7f53b74526bf6e77
shashikant kumar <shashi94goswami@gmail.com>
2025-03-11 22:01:25 +0530

    added infinite scroll with js

--- dom-manipulation/infinite-scroll/index.js
+++ dom-manipulation/infinite-scroll/index.js
@@ -1,16 +1,58 @@
-const API_BASE_URL = 'https://api.frontendexpert.io/api/fe/testimonials';
+const API_BASE_URL = 'https://api.frontendexpert.io/api/fe/testimonials?';
 
-(async () => {
-  try {
-    const response = await fetch(API_BASE_URL);
+const container = document.getElementById('testimonial-container');
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+let hasNext = false;
+let id = '';
+let canFetch = true;
+
+const addToDom = (res) => {
+    if (res && res.testimonials) {
+        const fragment = document.createDocumentFragment();
+        res.testimonials.forEach((item, index) => {
+            const p = document.createElement("p");
+            p.textContent = item.message;
+            p.classList.add("testimonial");
+            fragment.appendChild(p);
+
+           
+            if (index === res.testimonials.length - 1) {
+                id = item.id;
+            }
+        });
+
+        container.appendChild(fragment);
+        hasNext = res.hasNext;
+        canFetch = true; 
     }
+};
 
-    const data = await response.json();
-    console.log(data, "data");
-  } catch (error) {
-    console.error("Error fetching testimonials:", error.message);
-  }
-})();
+const fetchAPI = async () => {
+    if (!canFetch) return; // Prevent multiple fetches
+
+    canFetch = false; // Block further requests until this completes
+    const URL = id ? `${API_BASE_URL}limit=5&after=${id}` : `${API_BASE_URL}limit=5`;
+
+    try {
+        const response = await fetch(URL);
+        if (!response.ok) throw new Error('Network response was not ok');
+        
+        const data = await response.json();
+        addToDom(data);
+    } catch (error) {
+        console.error("Error fetching testimonials:", error);
+        canFetch = true; // Allow retrying in case of failure
+    }
+};
+
+function scrollListener() {
+    if (!canFetch || !hasNext) return;
+
+    const spaceLeftToScroll = container.scrollHeight - container.scrollTop - container.clientHeight;
+    if (spaceLeftToScroll <= 0) {
+        fetchAPI();
+    }
+}
+
+container.addEventListener("scroll", scrollListener);
+fetchAPI();
